cf8364299ff901b66b0e6a497947a0d2f3f81e59
hervala <hervala@gmail.com>
2020-12-04 23:06:19 +0200

    day 4

---
+++ AdventOfCode2020/Day05.cs
@@ -0,0 +1,36 @@
+﻿using CliFx;
+using CliFx.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+
+    [Command(nameof(Day05))]
+    public class Day05 : DayCommand, ICommand
+    {
+        public override ValueTask Part01(IConsole console)
+        {
+            console.Output.WriteLine($"{1}");
+            return default;
+        }
+
+        public override ValueTask Part02(IConsole console)
+        {
+            console.Output.WriteLine($"{1}");
+            return default;
+        }
+
+        private IEnumerable<string> ParseInput(string input)
+        {
+            return new List<string>();
+        }
+
+        private string day5Input = @"";
+
+    }
+}
