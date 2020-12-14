de44e7dd291bf619a392e49eb56f7827f8f6b1af
hervala <hervala@gmail.com>
2020-12-14 21:24:52 +0200

    day 13 and 14

--- AdventOfCode2020/Day13.cs
+++ AdventOfCode2020/Day13.cs
@@ -16,21 +16,39 @@ namespace AdventOfCode2020
     {
         public override ValueTask Part01(IConsole console)
         {
-            console.Output.WriteLine($"{1}");
+            var (earliestDepartTime, busTimes) = ParseInput(day13Input);
+            foreach (var bus in busTimes)
+            {
+                var sum = 0;
+                do
+                {
+                    sum += bus.Key;
+                } while (sum < earliestDepartTime);
+                busTimes[bus.Key] = sum - earliestDepartTime;
+            }
+            var earliestBus = busTimes.Aggregate((lowest, current) => current.Value < lowest.Value ? current : lowest);
+            console.Output.WriteLine($"{earliestBus.Key * earliestBus.Value}");
             return default;
         }
 
         public override ValueTask Part02(IConsole console)
         {
-            console.Output.WriteLine($"{1}");
+            var (earliestDepartTime, busTimes) = ParseInput(day13Input);
+            console.Output.WriteLine($"{"Chinese Remainder Theorem would solve this. Don't have time to learn number theory for now."}");
             return default;
         }
 
-        private void ParseInput(string input)
+        private (int earliestDepartTime, Dictionary<int, int> busTimes) ParseInput(string input)
         {
+            var lines = input.Split(Environment.NewLine).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            var earliestDepartTime = int.Parse(lines[0]);
+            var busTimes = lines[1].Split(',').Where(s => s != "x").ToDictionary(s => int.Parse(s), _ => 0);
+            return (earliestDepartTime, busTimes);
         }
 
-        private string day13Input = @"";
+        private string day13Input = @"1006605
+19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,883,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,x,x,797,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29
+";
 
     }
 }
