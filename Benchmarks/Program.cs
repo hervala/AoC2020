643d1d80257abcbb98d89f9b9703144ab59cbfac
hervala <hervala@gmail.com>
2020-12-16 23:09:31 +0200

    day 16

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -9,28 +9,36 @@ namespace ConsoleApp1
 {
     public class Benchmarks
     {
-        private Day14 day14;
-        private Day15 day15;
+        private Day14 Day14;
+        private Day15 Day15;
+        private Day16 Day16;
         private IConsole console;
 
         public Benchmarks()
         {
-            day14 = new Day14();
-            day15 = new Day15();
+            Day14 = new Day14();
+            Day15 = new Day15();
+            Day16 = new Day16();
             (console, _, _) = VirtualConsole.CreateBuffered();
         }
 
         [Benchmark]
-        public ValueTask Day14_Part1() => day14.Part01(console);
+        public ValueTask Day14_Part1() => Day14.Part01(console);
 
         [Benchmark]
-        public ValueTask Day14_Part2() => day14.Part02(console);
+        public ValueTask Day14_Part2() => Day14.Part02(console);
 
         [Benchmark]
-        public ValueTask Day15_Part1() => day15.Part01(console);
+        public ValueTask Day15_Part1() => Day15.Part01(console);
 
         [Benchmark]
-        public ValueTask Day15_Part2() => day15.Part02(console);
+        public ValueTask Day15_Part2() => Day15.Part02(console);
+
+        [Benchmark]
+        public ValueTask Day16_Part1() => Day16.Part01(console);
+
+        [Benchmark]
+        public ValueTask Day16_Part2() => Day16.Part02(console);
 
     }
 
